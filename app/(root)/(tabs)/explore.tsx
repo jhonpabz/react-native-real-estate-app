c6cbc32f1eec0b9461d2ba4fa33256c9ca323ec4
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2025-01-13 22:02:45 +0800

    add: pages

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const explore = () => {
+  return (
+    <View>
+      <Text>explore</Text>
+    </View>
+  );
+};
+export default explore;
---
+++ app/signIn.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+const signIn = () => {
+  return (
+    <View>
+      <Text>sign-in</Text>
+    </View>
+  );
+};
+export default signIn;
